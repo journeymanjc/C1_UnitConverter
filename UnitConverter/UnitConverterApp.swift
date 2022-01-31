5208267ed6f8c4218e86561486f263ce3dcabfb8
Jae Cho <jjaecho@gmail.com>
2022-01-31 15:22:31 -0500

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Jae Cho on 1/31/22.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
